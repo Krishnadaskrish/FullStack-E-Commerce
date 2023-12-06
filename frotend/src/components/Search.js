3620ae4446a64b370d87c6c6c380a60bb7c31394
hornto <krishnadas10.official@gmail.com>
2023-12-06 21:43:26 +0530

    search bar problem resolved

--- frotend/src/components/Search.js
+++ frotend/src/components/Search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MDBBtn,
     MDBCardBody,
     MDBCardTitle,
@@ -11,12 +11,33 @@ import { MDBBtn,
 } from 'mdb-react-ui-kit';
 
 import { useNavigate } from 'react-router-dom';
-import { product } from '../Products/Product';
+import { Axios } from '../App';
+
 
 
 
 const Search = ({searchTerm}) => {
     const ab = useNavigate();
+    const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  const fetchProducts = async () => {
+    try {
+      
+      const response = await Axios.get("api/users/products");
+      console.log("API Response:", response);
+  
+      if (response.status === 201) {
+        console.log("Updating products state...");
+        setProducts(response.data.data);
+      }
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+  
+  fetchProducts();
+},[]);
     
   return (
     <>
@@ -24,24 +45,24 @@ const Search = ({searchTerm}) => {
             {searchTerm ? (
   <div className="container mx-5">
     <MDBRow>
-      {product &&
-  product.filter((val) => {
+      {products &&
+  products.filter((val) => {
         if (searchTerm === "") {
           return true; // Return true to include all items when the search term is empty
         } else if (
-          val.name.toLowerCase().includes(searchTerm.toLowerCase())
+          val.title.toLowerCase().includes(searchTerm.toLowerCase())
         ) {
           return true; // Return true to include items that match the search term
         }
         return false; // Return false to exclude items that don't match the search term
       }).map((val) => (
-        <MDBCol md="4" key={val.id}>
+        <MDBCol md="4" key={val._id}>
           <MDBCard className="mt-3">
-            <MDBCardImage src={val.src} position="top" alt="..." />
+            <MDBCardImage src={val.image} position="top" alt="..." />
             <MDBCardBody>
-              <MDBCardTitle>{val.name} </MDBCardTitle>
+              <MDBCardTitle>{val.title} </MDBCardTitle>
               <MDBCardTitle>₹{val.price} </MDBCardTitle>
-              <MDBBtn  onClick={()=>ab(`/displayProduct/${val.id}`)}>View</MDBBtn>
+              <MDBBtn  onClick={()=>ab(`/displayProduct/${val._id}`)}>View</MDBBtn>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
